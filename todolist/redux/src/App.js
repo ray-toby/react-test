9700f7a17653c8e7844c420325e8a1ac5732f551
ray-toby <623501500@qq.com>
2019-08-22 15:38:45 +0800

    'redux管理todolist'

--- todolist/redux/src/App.js
+++ todolist/redux/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 
