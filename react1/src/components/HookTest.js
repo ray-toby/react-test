634b2ac8b29ebe93dbbe40bf382562ab35605924
fei.lei <fei.lei@audaque.com>
2020-04-20 15:03:26 +0800

    useState useEffect

--- react1/src/components/HookTest.js
+++ react1/src/components/HookTest.js
@@ -1,11 +1,36 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 // import PropTypes from 'prop-types';
+
+
+//自定义钩子  名称用’use‘开头
+function useAge() {
+    const [age, setAge] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setAge(20)
+        }, 2000);
+    });
+
+    return age;
+}
 
 export default function HookTest() {
     //hook使用
     const [count, setCount] = useState(0);
 
-    const [age] = useState(20);
+    //useEffect  副作用钩子  \每次渲染都会执行
+    useEffect(() => {
+        document.title = `你点击了${count}次`;
+    });
+    //如果仅执行一次，  传递第二个参数为[]
+    //componentDidMount   如果其他依赖状态变化  其他没有变化  设置第二个参数依赖  [count]
+    useEffect(() => {
+        //api调用
+        console.log('api调用')
+    }, []);
+
+    const age = useAge();
 
     const [fruit, setFruit] = useState('banana');
 
@@ -17,7 +42,7 @@ export default function HookTest() {
         <div>
             <p>点击了{count} 次</p>
             <button onClick={() => setCount(count + 1)}>点击</button>
-            <p>年龄{age}</p>
+            <p>年龄{age ? age : 'loading...'}</p>
             <p>选择的水果：{fruit}</p>
             <ul>
                 {fruits.map(f => (<li key={f} onClick={() => setFruit(f)} >{f}</li>))}
